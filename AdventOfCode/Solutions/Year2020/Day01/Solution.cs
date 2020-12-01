40a7ee44cb76b22587a2f51e54bbb368ec9e27a2
Geoffrey Hibbert <geoffrey.hibbert@moser-inc.com>
2020-12-01 09:36:16 -0500

    added day one part two solution

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -35,7 +35,7 @@ namespace AdventOfCode.Solutions.Year2020
                         break;
                     }
                 }
-                if(!keepGoing)
+                if (!keepGoing)
                 {
                     break;
                 }
@@ -45,7 +45,41 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var goal = 2020;
+            var lines = Input.Split("\n");
+            var firstNumber = 0;
+            var secondNumber = 0;
+            var thirdNumber = 0;
+            var numbers = lines.Select(l => int.Parse(l)).ToArray();
+            var keepGoing = true;
+            for (var i = 0; i < numbers.Count() - 3; i++)
+            {
+                firstNumber = numbers[i];
+                for (var j = i + 1; j < numbers.Count() - 2; j++)
+                {
+                    secondNumber = numbers[j];
+                    for (var k = j + 1; k < numbers.Count() - 1; k++)
+                    {
+                        thirdNumber = numbers[k];
+
+                        if (firstNumber + secondNumber + thirdNumber == goal)
+                        {
+                            keepGoing = false;
+                            break;
+                        }
+                    }
+                    if (!keepGoing)
+                    {
+                        break;
+                    }
+                }
+                if (!keepGoing)
+                {
+                    break;
+                }
+            }
+
+            return (firstNumber * secondNumber * thirdNumber).ToString();
         }
     }
 }

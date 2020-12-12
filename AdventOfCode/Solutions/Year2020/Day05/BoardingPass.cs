803692777aad9931168b21a0733d4533a654f32f
Geoffrey Hibbert <geoffrey.hibbert@moser-inc.com>
2020-12-12 09:50:47 -0500

    added day 5 solution

---
+++ AdventOfCode/Solutions/Year2020/Day05/BoardingPass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    internal class BoardingPass
+    {
+        public BoardingPass(string i)
+        {
+            var seatId = i.Replace('F', '0')
+                          .Replace('B', '1')
+                          .Replace('L', '0')
+                          .Replace('R', '1');
+            SeatId = Convert.ToInt32(seatId, 2);
+        }
+
+        public int SeatId { get; internal set; }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -1,22 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day05 : ASolution
     {
+        public IEnumerable<BoardingPass> BoardingPasses { get; }
 
         public Day05() : base(05, 2020, "")
         {
-
+            var input = Input.SplitByNewline();
+            BoardingPasses = input.Select(i => new BoardingPass(i));
         }
+
 
         protected override string SolvePartOne()
         {
-            return null;
+            return BoardingPasses.Max(b => b.SeatId).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var passes = BoardingPasses.OrderBy(b => b.SeatId).ToList();
+            for (var i = 0; i < passes.Count() - 1; i++)
+            {
+                if(passes[i].SeatId +2 == passes[i+1].SeatId)
+                {
+                    var targetSeatId = passes[i].SeatId + 1;
+                    return targetSeatId.ToString();
+                }
+            }
+            return "Seat not found!";
         }
     }
 }

f88a12ee03d8cd64fb2e1aea32ec37d7e01ff90d
Geoffrey Hibbert <geoffrey.hibbert@moser-inc.com>
2020-12-18 22:15:20 -0500

    added day 6 solutions and tests and some cleanup

---
+++ AdventOfCode/Models/SurveyGroup.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Solutions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    public class SurveyGroup
+    {
+        public IEnumerable<SurveyUser> SurveyUsers { get; }
+        public int DistinctTotal => SurveyUsers.SelectMany(u => u.SurveyElections)
+                                               .Distinct()
+                                               .Count();
+        public IEnumerable<char> ElectedByAll => SurveyUsers.SelectMany(u => u.SurveyElections)
+                                                 .GroupBy(c => c)
+                                                 .Where(g => g.Count() == SurveyUsers.Count())
+                                                 .Select(g => g.Key);
+        public SurveyGroup(string data)
+        {
+            SurveyUsers = data.SplitByNewline().Select(u => new SurveyUser(u));
+        }
+
+    }
+}
---
+++ AdventOfCode/Models/SurveyUser.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode.Solutions.Year2020
+{
+    public class SurveyUser
+    {
+        public char[] SurveyElections { get; }
+
+        public SurveyUser(string u)
+        {
+            SurveyElections = u.ToCharArray();
+        }
+
+    }
+}

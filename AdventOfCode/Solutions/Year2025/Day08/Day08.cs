787b21ca1a0b980e5f244b9b6247b4c685c124fc
Geoffrey Hibbert <stereotypeaura@gmail.com>
2025-12-01 16:57:36 -0500

    Generated 2025 solution files

---
+++ AdventOfCode/Solutions/Year2025/Day08/Day08.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Solutions.Year2025
+{
+
+    class Day08 : ASolution
+    {
+
+        public Day08() : base(08, 2025, "")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}

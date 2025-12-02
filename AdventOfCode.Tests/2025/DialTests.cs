d9ea2dff60f11f27f4fc184f0b40e9c2579645f3
Geoffrey Hibbert <stereotypeaura@gmail.com>
2025-12-01 23:45:34 -0500

    2025 Day 01 Part 2 tests

--- AdventOfCode.Tests/2025/DialTests.cs
+++ AdventOfCode.Tests/2025/DialTests.cs
@@ -10,24 +10,35 @@ namespace AdventOfCode.Tests
     [TestClass]
     public class DialTests
     {
+        private string Input = "L68\n" +
+                               "L30\n" +
+                               "R48\n" +
+                               "L5 \n" +
+                               "R60\n" +
+                               "L55\n" +
+                               "L1 \n" +
+                               "L99\n" +
+                               "R14\n" +
+                               "L82\n";
 
         [TestMethod]
         public void Test_DayOnePartOneExample()
         {
-            var input = "L68\n" +
-                        "L30\n" +
-                        "R48\n" +
-                        "L5 \n" +
-                        "R60\n" +
-                        "L55\n" +
-                        "L1 \n" +
-                        "L99\n" +
-                        "R14\n" +
-                        "L82\n";
-            var directions = input.SplitByNewline();
+            var directions = Input.SplitByNewline();
             var dial = new Dial();
             dial.ProcessDirections(directions);
-            Assert.AreEqual(3, dial.ZeroTriggerCount);
+            Assert.AreEqual(3, dial.ZeroStopCount);
+        }
+
+        [TestMethod]
+        public void Test_DayOnePartTwoExample()
+        { 
+            var directions = Input.SplitByNewline();
+            var dial = new Dial();
+            dial.ProcessDirections(directions);
+            Assert.AreEqual(3, dial.ZeroStopCount);
+            Assert.AreEqual(3, dial.ZeroPassCount);
+            Assert.AreEqual(6, dial.ZeroTotalCount);
         }
     }
 }

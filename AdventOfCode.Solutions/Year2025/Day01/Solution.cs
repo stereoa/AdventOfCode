812f5748ef0ec39c31ffb0ef8ad64c98b1be7450
Geoffrey Hibbert <stereotypeaura@gmail.com>
2025-12-01 22:22:23 -0500

    Day 01 Part 1 2025

--- AdventOfCode.Solutions/Year2025/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2025/Day01/Solution.cs
@@ -1,12 +1,17 @@
+using AdventOfCode.AdventOfCode.Solutions.Models;
+
 namespace AdventOfCode.Solutions.Year2025.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2025, "") { }
+    public Solution() : base(01, 2025, "Secret Entrance") { }
 
     protected override string? SolvePartOne()
     {
-        return null;
+        var directions = Input.SplitByNewline();
+        var dial = new Dial();
+        dial.ProcessDirections(directions);
+        return dial.ZeroTriggerCount.ToString();
     }
 
     protected override string? SolvePartTwo()

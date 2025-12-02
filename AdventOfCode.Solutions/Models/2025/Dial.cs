6407869e2d0dc5e7aa83aa5a5d1be6ed417464d7
Geoffrey Hibbert <stereotypeaura@gmail.com>
2025-12-01 22:11:36 -0500

    Added Dial and tests

---
+++ AdventOfCode.Solutions/Models/2025/Dial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.AdventOfCode.Solutions.Models
+{
+    public class Dial
+    {
+        public int CurrentPosition { get; private set; } = 50;
+        public int ZeroTriggerCount { get; private set; }
+
+        public void ProcessDirections(string[] directions)
+        {
+            foreach (var direction in directions)
+            {
+                if (string.IsNullOrWhiteSpace(direction)) continue;
+                var turnDirection = direction[0];
+                if (!int.TryParse(direction[1..].Trim(), out int turnAmount))
+                {
+                    throw new ArgumentException($"Invalid turn amount in direction: {direction}");
+                }
+                if (turnDirection == 'L')
+                {
+                    CurrentPosition -= turnAmount;
+                }
+                else if (turnDirection == 'R')
+                {
+                    CurrentPosition += turnAmount;
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid turn direction in direction: {direction}");
+                }
+                while (CurrentPosition < 0)
+                {
+                    CurrentPosition += 100;
+                }
+                while (CurrentPosition >= 100)
+                {
+                    CurrentPosition -= 100;
+                }
+                if (CurrentPosition == 0)
+                {
+                    ZeroTriggerCount++;
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode.Tests/2025/DialTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Models;
+using System.Linq;
+using AdventOfCode.Solutions;
+using AdventOfCode.Solutions.Utils;
+using AdventOfCode.AdventOfCode.Solutions.Models;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass]
+    public class DialTests
+    {
+
+        [TestMethod]
+        public void Test_DayOnePartOneExample()
+        {
+            var input = "L68\n" +
+                        "L30\n" +
+                        "R48\n" +
+                        "L5 \n" +
+                        "R60\n" +
+                        "L55\n" +
+                        "L1 \n" +
+                        "L99\n" +
+                        "R14\n" +
+                        "L82\n";
+            var directions = input.SplitByNewline();
+            var dial = new Dial();
+            dial.ProcessDirections(directions);
+            Assert.AreEqual(3, dial.ZeroTriggerCount);
+        }
+    }
+}

b59e5f059368a1a20802cd17be8aafe575cd14b1
Geoffrey Hibbert <stereotypeaura@gmail.com>
2025-12-23 15:45:09 -0500

    Added day puzzle name for day 02 (2025)

--- AdventOfCode.Solutions/Year2025/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2025/Day02/Solution.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode.Solutions.Year2025.Day02;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(02, 2025, "") { }
+    public Solution() : base(02, 2025, "Gift Shop") { }
 
     protected override string? SolvePartOne()
     {
